97cc045c893a04156c23d4f52cfbef5381c32aa3
blanktourist <101967933+blanktourist@users.noreply.github.com>
2022-04-04 00:10:28 -0700

    Add description to ThreeSum

--- src/leetcode75/ThreeSum.java
+++ src/leetcode75/ThreeSum.java
@@ -4,6 +4,26 @@ import java.util.List;
 
 public class ThreeSum {
     
+    /**
+     * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+     * 
+     * Notice that the solution set must not contain duplicate triplets.
+     * 
+     * Ex. 1
+     * Input: nums = [-1,0,1,2,-1,-4]
+     * Output: [[-1,-1,2],[-1,0,1]]
+     * 
+     * Ex. 2
+     * Input: nums = []
+     * Output: []
+     * 
+     * Ex. 3
+     * Input: nums = [0]
+     * Output: []
+     * 
+     * @param arr
+     * @return
+     */
     public static List<List<Integer>> returnTriplets(int[] arr) {
         List<List<Integer>> results = new ArrayList<>();
 
