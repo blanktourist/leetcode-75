e1b4d81833017d6dd0d0a24929c7d7cb70d8a91f
blanktourist <101967933+blanktourist@users.noreply.github.com>
2022-04-10 21:44:28 -0700

    Refactor ListNode building

--- src/leetcode75/Utils/LinkedListUtils.java
+++ src/leetcode75/Utils/LinkedListUtils.java
@@ -5,31 +5,21 @@ import models.ListNode;
 public class LinkedListUtils {
 
     public static ListNode buildGenericLinkedList() {
-        ListNode n0 = new ListNode(1);
-        ListNode n1 = new ListNode(2);
-        ListNode n2 = new ListNode(3);
-        ListNode n3 = new ListNode(4);
         ListNode n4 = new ListNode(5);
-
-        n0.next = n1;
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
+        ListNode n3 = new ListNode(4, n4);
+        ListNode n2 = new ListNode(3, n3);
+        ListNode n1 = new ListNode(2, n2);
+        ListNode n0 = new ListNode(1, n1);
         
         return n0;
     }
 
     public static ListNode buildCycleLinkedList() {
-        ListNode n0 = new ListNode(1);
-        ListNode n1 = new ListNode(2);
-        ListNode n2 = new ListNode(3);
-        ListNode n3 = new ListNode(4);
         ListNode n4 = new ListNode(5);
-
-        n0.next = n1;
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
+        ListNode n3 = new ListNode(4, n4);
+        ListNode n2 = new ListNode(3, n3);
+        ListNode n1 = new ListNode(2, n2);
+        ListNode n0 = new ListNode(1, n1);
         n4.next = n0;
         
         return n0;
