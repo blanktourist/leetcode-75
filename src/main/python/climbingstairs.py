719b818fc03deb79085dd9df291357c45791f650
blanktourist <101967933+blanktourist@users.noreply.github.com>
2022-06-23 12:31:40 -0700

    Add py solution to Climbing Stairs

---
+++ src/main/python/climbingstairs.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.climbStairs(2))
+    print(s.climbStairs(3))
+    print(s.climbStairs(4))
+    print(s.climbStairs(5))
+    print(s.climbStairs(6))
+    print(s.climbStairs(7))
